73b8ca28c35b8a46e5689cc0401238f4bd72c203
Anatoly <povly19995@gmail.com>
2022-01-29 23:39:35 +0300

    add modals

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,39 @@
-let minig = 1;
-console.log(minig);
-document;
+document.addEventListener('DOMContentLoaded', ()=>{
+    const modalShows = document.querySelectorAll('.modal__show');
+
+    function hideActiveModals(){
+        const modals = document.querySelectorAll('.modal.active');
+        if (modals[0]){
+            modals.forEach((modal)=>{
+                modal.classList.remove('active');
+            })
+        }
+    }
+
+    function hiddenBodyOverflow(){
+        document.body.style.overflow = 'hidden';
+    }
+    function clearBodyOverflow(){
+        document.body.style.overflow = '';
+    }
+
+    modalShows.forEach((show)=>{
+        show.addEventListener('click', ()=>{
+            hideActiveModals();
+            let dataModal = show.getAttribute('data-modal');
+            let modal = document.querySelector('.modal__' + dataModal);
+            modal.classList.add('active');
+            hiddenBodyOverflow();
+        })
+    })
+
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach((modal)=>{
+        modal.addEventListener('click',(e)=>{
+            if (e.target === modal){
+                modal.classList.remove('active');
+                clearBodyOverflow();
+            }
+        })
+    })
+})
